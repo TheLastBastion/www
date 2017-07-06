ea2d0a0d96d0692716184443bf11c74eb538b3ec
Cody James Casey <codyjamescasey@gmail.com>
2017-07-06 18:37:14 -0400

    fixing jsx lint issue

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './app.css';
 
 export default function () {
-  return(
+  return (
     <div className="app">
       <div className="app-header">
         <h2>The Last Bastion</h2>
