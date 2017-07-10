c1c2dbced35e644c38fcb7e7748370e229bc0452
Cody James Casey <codyjamescasey@gmail.com>
2017-07-10 15:08:56 -0400

    babel parser for eslint, pulling header and nav to own components. Better nav w/ awesome hamburger button for mobile header. stubs for some other routes.

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
   BrowserRouter as Router,
   Route,
-  Link,
   Switch,
 } from 'react-router-dom';
-
 import createHashHistory from 'history/createHashHistory';
+import debounce from 'lodash/debounce';
+
+import Header from './header/header';
+import NavMenu from './nav-menu/nav-menu';
 
 import Splash from './splash';
 
@@ -16,15 +18,19 @@ import SpaceEngineers from './games/space-engineers';
 import Minecraft from './games/minecraft';
 import Rust from './games/rust';
 
+import Gallery from './gallery';
+import Forum from './forum';
+import CodeOfConduct from './coc';
+
 import './app.css';
 
 const history = createHashHistory();
 
-const AppRoute = ({ component: Component, ...rest }) => (
+const AppRoute = ({ component: RouteComponent, ...rest }) => (
   <Route
     {...rest}
     render={props => (
-      <Component className="app__route" {...props} />
+      <RouteComponent className="app__route" {...props} />
     )}
   />
 );
@@ -33,25 +39,89 @@ AppRoute.propTypes = {
   component: PropTypes.func.isRequired,
 };
 
-export default function () {
-  return (
-    <Router history={history}>
-      <div className="app">
-        <div className="app__header">
-          <h1>The Last Bastion</h1>
-          <ul>
-            <li><Link className="app__nav-link" to="/">Home</Link></li>
-            <li><Link className="app__nav-link" to="/games">Games</Link></li>
-          </ul>
-        </div>
-        <Switch>
-          <AppRoute exact path="/" component={Splash} />
-          <AppRoute exact path="/games" component={Games} />
-          <AppRoute path="/games/space-engineers" component={SpaceEngineers} />
-          <AppRoute path="/games/minecraft" component={Minecraft} />
-          <AppRoute path="/games/rust" component={Rust} />
-        </Switch>
-      </div>
-    </Router>
-  );
+function getIsSmallScreen() {
+  return window.innerWidth < 768;
 }
+
+class App extends Component {
+  constructor() {
+    super();
+
+    const isSmallScreen = getIsSmallScreen();
+    this.state = {
+      isSmallScreen,
+      showNavMenu: !isSmallScreen,
+    };
+
+    this.updateDimensions = debounce(this.updateDimensions.bind(this), 150);
+    this.handleItemSelected = this.handleItemSelected.bind(this);
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
+  updateDimensions() {
+    const isSmallScreen = getIsSmallScreen();
+    this.setState({
+      isSmallScreen,
+      showNavMenu: !isSmallScreen ? true : this.state.showNavMenu,
+    });
+  }
+
+  handleItemSelected() {
+    if (this.state.isSmallScreen) {
+      this.setState({
+        showNavMenu: false,
+      });
+    }
+  }
+
+  toggleMenu() {
+    this.setState({
+      showNavMenu: !this.state.showNavMenu,
+    });
+  }
+
+  render() {
+    const { isSmallScreen, showNavMenu } = this.state;
+
+    const navMenu = showNavMenu ? (
+      <NavMenu className="app__nav-menu" handleItemSelected={this.handleItemSelected} />
+    ) : null;
+
+    return (
+      <Router history={history}>
+        <div className="app">
+          <Header
+            showNavMenu={showNavMenu}
+            toggleMenu={this.toggleMenu}
+            isSmallScreen={isSmallScreen}
+          />
+          <div className="app__content">
+            {navMenu}
+            <div className="app__active-route">
+              <Switch>
+                <AppRoute exact path="/" component={Splash} />
+                <AppRoute exact path="/games" component={Games} />
+                <AppRoute path="/games/space-engineers" component={SpaceEngineers} />
+                <AppRoute path="/games/minecraft" component={Minecraft} />
+                <AppRoute path="/games/rust" component={Rust} />
+                <AppRoute path="/gallery" component={Gallery} />
+                <AppRoute path="/forum" component={Forum} />
+                <AppRoute path="/coc" component={CodeOfConduct} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/forum.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import ComingSoon from './coming-soon';
+
+import './forum.css';
+
+function Forum({ className }) {
+  const blockClassName = classNames('forum', className);
+  return (
+    <div className={blockClassName}>
+      <h2>This is the forum page</h2>
+      <ComingSoon className="forum__coming-soon" />
+    </div>
+  );
+}
+
+Forum.propTypes = {
+  className: PropTypes.string,
+};
+
+Forum.defaultProps = {
+  className: '',
+};
+
+export default Forum;
